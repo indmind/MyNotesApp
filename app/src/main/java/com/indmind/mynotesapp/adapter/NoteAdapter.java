5b3a2e00ccdcb4b8749de8ceb996712c99f8d064
indmind <sattanationmail@gmail.com>
2019-07-01 09:16:04 +0700

    initial commit

---
+++ app/src/main/java/com/indmind/mynotesapp/adapter/NoteAdapter.java
@@ -0,0 +1,105 @@
+package com.indmind.mynotesapp.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.indmind.mynotesapp.CustomOnClickListener;
+import com.indmind.mynotesapp.NoteAddUpdateActivity;
+import com.indmind.mynotesapp.R;
+import com.indmind.mynotesapp.entity.Note;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+    private ArrayList<Note> listNotes = new ArrayList<>();
+    private Activity activity;
+
+    public NoteAdapter(Activity activity) {
+        this.activity = activity;
+    }
+
+    public ArrayList<Note> getListNotes() {
+        return listNotes;
+    }
+
+    public void setListNotes(ArrayList<Note> listNotes) {
+        if (listNotes.size() > 0) {
+            this.listNotes.clear();
+        }
+
+        this.listNotes.addAll(listNotes);
+
+        notifyDataSetChanged();
+    }
+
+    public void addItem(Note note) {
+        this.listNotes.add(note);
+        notifyItemInserted(listNotes.size() - 1);
+    }
+
+    public void updateItem(int position, Note note) {
+        this.listNotes.set(position, note);
+        notifyItemChanged(position, note);
+    }
+
+    public void removeItem(int position) {
+        this.listNotes.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, listNotes.size());
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_note, viewGroup, false);
+
+        return new NoteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int i) {
+        Note note = listNotes.get(i);
+
+        holder.tvTitle.setText(note.getTitle());
+        holder.tvDate.setText(note.getDate());
+        holder.tvDescription.setText(note.getDescription());
+
+        holder.cvNote.setOnClickListener(new CustomOnClickListener(i, new CustomOnClickListener.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(View view, int position) {
+                Intent intent = new Intent(activity, NoteAddUpdateActivity.class);
+
+                intent.putExtra(NoteAddUpdateActivity.EXTRA_POSITION, position);
+                intent.putExtra(NoteAddUpdateActivity.EXTRA_NOTE, listNotes.get(position));
+
+                activity.startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_UPDATE);
+            }
+        }));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listNotes.size();
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvDescription, tvDate;
+        CardView cvNote;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.tv_item_title);
+            tvDescription = itemView.findViewById(R.id.tv_item_description);
+            tvDate = itemView.findViewById(R.id.tv_item_date);
+            cvNote = itemView.findViewById(R.id.cv_item_note);
+        }
+    }
+}
---
+++ app/src/main/java/com/indmind/mynotesapp/db/NoteHelper.java
@@ -0,0 +1,111 @@
+package com.indmind.mynotesapp.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.indmind.mynotesapp.entity.Note;
+
+import java.util.ArrayList;
+
+import static android.provider.BaseColumns._ID;
+
+public class NoteHelper {
+    private static final String DATABASE_TABLE = DatabaseContract.TABLE_NOTE;
+
+    private static DatabaseHelper databaseHelper;
+    private static NoteHelper INSTANCE;
+
+    private static SQLiteDatabase database;
+
+    private NoteHelper(Context context) {
+        databaseHelper = new DatabaseHelper(context);
+    }
+
+    public static NoteHelper getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (SQLiteOpenHelper.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new NoteHelper(context);
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    public void open() throws SQLException  {
+        database = databaseHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        databaseHelper.close();
+
+        if (database.isOpen()) {
+            database.close();
+        }
+    }
+
+    public ArrayList<Note> getAllNotes() {
+        ArrayList<Note> arrayList = new ArrayList<>();
+
+        Cursor cursor = database.query(DATABASE_TABLE,
+                null,
+                null,
+                null,
+                null,
+                null,
+                _ID + " ASC",
+                null
+        );
+
+        cursor.moveToFirst();
+
+        Note note;
+
+        if (cursor.getCount() > 0) {
+            do {
+                note = new Note();
+
+                note.setId(cursor.getInt(cursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID)));
+                note.setTitle(cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE)));
+                note.setDescription(cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION)));
+                note.setDate(cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE)));
+
+                arrayList.add(note);
+                cursor.moveToNext();
+            } while (!cursor.isAfterLast());
+        }
+
+        cursor.close();
+
+        return arrayList;
+    }
+
+    public long insertNote(Note note) {
+        ContentValues args = new ContentValues();
+
+        args.put(DatabaseContract.NoteColumns.TITLE, note.getTitle());
+        args.put(DatabaseContract.NoteColumns.DESCRIPTION, note.getDescription());
+        args.put(DatabaseContract.NoteColumns.DATE, note.getDate());
+
+        return database.insert(DATABASE_TABLE, null, args);
+    }
+
+    public int updateNote(Note note) {
+        ContentValues args = new ContentValues();
+
+        args.put(DatabaseContract.NoteColumns.TITLE, note.getTitle());
+        args.put(DatabaseContract.NoteColumns.DESCRIPTION, note.getDescription());
+        args.put(DatabaseContract.NoteColumns.DATE, note.getDate());
+
+        return database.update(DATABASE_TABLE, args, _ID + "= '" + note.getId() + "'", null);
+    }
+
+    public int deleteNote(int id) {
+        return database.delete(DatabaseContract.TABLE_NOTE,  _ID + " = '" + id + "'", null);
+    }
+}
